b076e48869a8592b356f3610d97d973a700c5de0
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2022-01-04 02:35:56 -0300

    Create task model and home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
 
-void main() {
+part 'main.g.dart';
+
+const String tasksBoxName = "tasks";
+
+@HiveType(typeId: 0)
+class Task {
+  @HiveField(0)
+  String name;
+
+  @HiveField(1)
+  String description;
+
+  @HiveField(2)
+  bool isDone;
+
+  Task(this.name, this.description, this.isDone);
+}
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter<Task>(TaskAdapter());
+  await Hive.openBox<Task>(tasksBoxName);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,92 +39,287 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHome> createState() => _MyHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomeState extends State<MyHome> {
+  final controller = SheetController();
+  bool isSelected = false;
+  List<int> selectedTiles = [];
+  var _temporaryBox;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void checkAndSelect(int index) {
+    if (selectedTiles.contains(index)) {
+      selectedTiles.remove(index);
+    } else {
+      selectedTiles.add(index);
+    }
+  }
+
+  void deleteTasks() async {
+    for (var index in selectedTiles) {
+      await _temporaryBox.deleteAt(index);
+    }
+
+    selectedTiles = [];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+    return WillPopScope(
+      onWillPop: () async {
+        if (selectedTiles.isNotEmpty) {
+          setState(() {
+            selectedTiles = [];
+          });
+        }
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("To-Do App"),
+          actions: [
+            selectedTiles.isNotEmpty
+                ? IconButton(
+                    onPressed: () => deleteTasks(),
+                    icon: const Icon(Icons.delete_outlined))
+                : IconButton(
+                    icon: const Icon(Icons.settings_outlined), onPressed: () {})
           ],
         ),
+        body: ValueListenableBuilder(
+          valueListenable: Hive.box<Task>(tasksBoxName).listenable(),
+          builder: (BuildContext context, Box<Task> box, Widget? child) {
+            _temporaryBox = box;
+
+            if (box.values.isEmpty) {
+              return const Center(
+                child: Text("Sem tarefas"),
+              );
+            }
+
+            return Column(
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: box.values.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Task? currentTask = box.getAt(index);
+
+                    return !currentTask!.isDone ? ListTile(
+                      selectedTileColor: Colors.grey.shade200,
+                      selected: selectedTiles.contains(index),
+                      onLongPress: () {
+                        setState(() {
+                          checkAndSelect(index);
+                        });
+                      },
+                      onTap: () {
+                        setState(() {
+                          if (selectedTiles.isNotEmpty) {
+                            checkAndSelect(index);
+                          }
+                        });
+                      },
+                      title: Text(currentTask.name),
+                      subtitle: currentTask.description.isNotEmpty
+                          ? Text(currentTask.description)
+                          : null,
+                      leading: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if (selectedTiles.isNotEmpty) {
+                                checkAndSelect(index);
+                              } else {
+                                currentTask.isDone = !currentTask.isDone;
+                              }
+                            });
+                          },
+                          icon: selectedTiles.contains(index)
+                              ? const Icon(Icons.circle)
+                              : currentTask.isDone
+                                  ? const Icon(
+                                      Icons.check_circle_outline,
+                                      color: Colors.green,
+                                    )
+                                  : const Icon(Icons.circle_outlined)),
+                    ) : Container();
+                  },
+                ),
+                Divider(),
+                Row(
+                  children: const [
+                    Text("Tarefas completadas")
+                  ],
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: box.values.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Task? currentTask = box.getAt(index);
+
+                    return currentTask!.isDone ? ListTile(
+                      selectedTileColor: Colors.grey.shade200,
+                      selected: selectedTiles.contains(index),
+                      onLongPress: () {
+                        setState(() {
+                          checkAndSelect(index);
+                        });
+                      },
+                      onTap: () {
+                        setState(() {
+                          if (selectedTiles.isNotEmpty) {
+                            checkAndSelect(index);
+                          }
+                        });
+                      },
+                      title: Text(currentTask.name),
+                      subtitle: currentTask.description.isNotEmpty
+                          ? Text(currentTask.description)
+                          : null,
+                      leading: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if (selectedTiles.isNotEmpty) {
+                                checkAndSelect(index);
+                              } else {
+                                currentTask.isDone = !currentTask.isDone;
+                              }
+                            });
+                          },
+                          icon: selectedTiles.contains(index)
+                              ? const Icon(Icons.circle)
+                              : currentTask.isDone
+                                  ? const Icon(
+                                      Icons.check_circle_outline,
+                                      color: Colors.green,
+                                    )
+                                  : const Icon(Icons.circle_outlined)),
+                    ) : Container();
+                  },
+                )
+              ],
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async => showBottomSheetDialog(context),
+          shape: const RoundedRectangleBorder(
+              side: BorderSide(color: Color(0xFF515CC6), width: 2),
+              borderRadius: BorderRadius.all(Radius.circular(50))),
+          backgroundColor: const Color(0xFF473FA0),
+          child: const Icon(Icons.add),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<void> showBottomSheetDialog(BuildContext context) async {
+    TextEditingController _newTaskController = TextEditingController();
+    TextEditingController _taskDetailsController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
+
+    await showSlidingBottomSheet(context, builder: (context) {
+      return SlidingSheetDialog(
+        cornerRadius: 15,
+        controller: controller,
+        duration: const Duration(milliseconds: 500),
+        snapSpec: const SnapSpec(
+          snap: true,
+          initialSnap: 0.3,
+        ),
+        scrollSpec: const ScrollSpec(
+          showScrollbar: true,
+        ),
+        minHeight: 200,
+        isDismissable: true,
+        dismissOnBackdropTap: true,
+        isBackdropInteractable: true,
+        onDismissPrevented: (backButton, backDrop) async {
+          HapticFeedback.heavyImpact();
+
+          if (backButton || backDrop) {
+            const duration = Duration(milliseconds: 300);
+            await controller.snapToExtent(0.3,
+                duration: duration, clamp: false);
+          }
+        },
+        builder: (context, state) => Material(
+          child: Form(
+            key: formKey,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
+              child: Wrap(
+                runSpacing: 15,
+                children: [
+                  TextFormField(
+                    controller: _newTaskController,
+                    maxLines: null,
+                    textCapitalization: TextCapitalization.sentences,
+                    style: GoogleFonts.getFont("Inter", fontSize: 16),
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                      labelText: "Nova tarefa",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Este campo é necessário';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _taskDetailsController,
+                    maxLines: null,
+                    style: GoogleFonts.getFont("Inter", fontSize: 16),
+                    decoration: const InputDecoration(
+                      labelText: "Detalhes",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        footerBuilder: (context, state) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(
+                width: 120,
+                child: TextButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      Box<Task> tasksBox = Hive.box<Task>(tasksBoxName);
+                      tasksBox.add(Task(_newTaskController.text.trim(),
+                          _taskDetailsController.text.trim(), false));
+                    }
+                  },
+                  child: Text(
+                    "Salvar",
+                    style: GoogleFonts.getFont("Inter", fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    });
   }
 }
