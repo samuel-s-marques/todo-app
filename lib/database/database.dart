86e1fd5bf966342fe71caadfe43baa6d9d5fb27e
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2022-01-07 22:36:22 -0300

    Create database.dart

---
+++ lib/database/database.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+part 'database.g.dart';
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
+
+@DriftDatabase(
+  include: {'tables.drift'}
+)
+class MyDb extends _$MyDb {
+  MyDb() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  @override
+  MigrationStrategy get migration => MigrationStrategy(
+    onCreate: (migrator) async {
+      await customStatement("PRAGMA foreign_keys = ON");
+      return migrator.createAll();
+    }
+  );
+}
