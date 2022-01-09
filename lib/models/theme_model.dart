1b1ba989ba265b47e43a8f9924c4e63bcc7c7db7
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2022-01-09 15:29:09 -0300

    Create theme_model.dart

---
+++ lib/models/theme_model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/theme_preferences.dart';
+
+class ThemeModel extends ChangeNotifier {
+  late bool _isDark;
+  late ThemePreferences _preferences;
+  bool get isDark => _isDark;
+
+  getPreferences() async {
+    _isDark = await _preferences.getTheme();
+    notifyListeners();
+  }
+
+  set isDark(bool value) {
+    _isDark = value;
+    _preferences.setTheme(value);
+    notifyListeners();
+  }
+
+  ThemeModel() {
+    _isDark = false;
+    _preferences = ThemePreferences();
+    getPreferences();
+  }
+}
