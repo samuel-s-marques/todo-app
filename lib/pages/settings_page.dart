271b32a7477869fa1592e6a1274a0689b94f9983
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2022-01-09 14:22:47 -0300

    Add dialog widget to ensure deleting data

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:todoapp/database/database.dart';
@@ -13,61 +14,104 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
+    bool lightMode = false;
+    bool automaticDarkMode = true;
+
     return Scaffold(
       appBar: AppBar(),
       body: SettingsList(
         sections: [
           SettingsSection(
-            title: Text('Configurações'),
+            title: const Text('Configurações'),
             tiles: [
               SettingsTile.navigation(
-                leading: Icon(Icons.language_outlined),
-                title: Text("Linguagem"),
-                value: Text("Inglês"),
+                leading: const Icon(Icons.language_outlined),
+                title: const Text("Linguagem"),
+                value: const Text("Inglês"),
               ),
               SettingsTile.navigation(
-                leading: Icon(Icons.access_time_outlined),
-                title: Text("Padrão de hora"),
-                value: Text("24H"),
+                leading: const Icon(Icons.access_time_outlined),
+                title: const Text("Padrão de hora"),
+                value: const Text("24H"),
               ),
               SettingsTile.switchTile(
-                leading: Icon(Icons.light_mode_outlined),
-                initialValue: true,
-                onToggle: (bool value) {},
-                title: Text("Tema claro"),
+                leading: const Icon(Icons.light_mode_outlined),
+                initialValue: lightMode,
+                onToggle: (bool value) {
+                  lightMode = value;
+                },
+                title: const Text("Tema claro"),
               ),
               SettingsTile.switchTile(
-                leading: Icon(Icons.dark_mode_outlined),
-                initialValue: true,
-                onToggle: (bool value) {},
-                title: Text("Tema escuro automático"),
+                leading: const Icon(Icons.dark_mode_outlined),
+                initialValue: automaticDarkMode,
+                onToggle: (bool value) {
+                  automaticDarkMode = value;
+                },
+                title: const Text("Tema escuro automático"),
               ),
             ],
           ),
           SettingsSection(
-            title: Text("Misc."),
+            title: const Text("Misc."),
             tiles: [
               SettingsTile.navigation(
-                leading: Icon(Icons.info_outline),
-                title: Text("Sobre"),
+                leading: const Icon(Icons.info_outline),
+                title: const Text("Sobre"),
               ),
               SettingsTile.navigation(
-                leading: Icon(Icons.article_outlined),
-                title: Text("Termos de uso"),
+                leading: const Icon(Icons.article_outlined),
+                title: const Text("Termos de uso"),
               ),
               SettingsTile.navigation(
-                leading: Icon(Icons.copyright_outlined),
-                title: Text("Licença"),
+                leading: const Icon(Icons.copyright_outlined),
+                title: const Text("Licença"),
               ),
             ],
           ),
           SettingsSection(
-            title: Text("Perigo"),
+            title: const Text("Perigo"),
             tiles: [
               SettingsTile(
-                leading: Icon(Icons.dangerous_outlined),
-                title: Text("Apagar todos os dados"),
+                leading: const Icon(Icons.dangerous_outlined),
+                title: const Text("Apagar todos os dados"),
                 onPressed: (context) {
+                  showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(
+                      title: const Text("Tem certeza que quer deletar todos os dados?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Cancelar",
+                            style: GoogleFonts.getFont(
+                              "Inter",
+                              fontWeight: FontWeight.w600,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Provider.of<MyDb>(context, listen: false).deleteAllFolders();
+                            Provider.of<MyDb>(context, listen: false).deleteAllTasks();
+
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Apagar",
+                            style: GoogleFonts.getFont(
+                              "Inter",
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
                 },
               ),
             ],
