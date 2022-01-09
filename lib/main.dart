b6967890c1fcaa02a3e2563139d0604de2db449a
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2022-01-09 00:09:47 -0300

    Add dark and light themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:todoapp/database/database.dart';
 import 'package:todoapp/pages/folders_page.dart';
@@ -33,9 +34,68 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        dialogBackgroundColor: const Color(0xFFFAFAFA),
+        textTheme: TextTheme(
+          headline1: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+            color: const Color(0xFF575767),
+          ),
+          headline2: GoogleFonts.getFont("Inter",
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: const Color(0xFFB9B9BE),
+              decoration: TextDecoration.lineThrough),
+          subtitle1: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: const Color(0xFFB9B9BE),
+          ),
+        ),
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        cardColor: const Color(0xFF141419),
+        scaffoldBackgroundColor: const Color(0xFF141419),
+        dialogBackgroundColor: const Color(0xFF303030),
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 16,
+            color: const Color(0xFFB9B9BE),
+          ),
+          fillColor: Colors.blue,
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
+        textTheme: TextTheme(
+          headline1: GoogleFonts.getFont("Inter",
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: const Color(0xFFDADADA)),
+          headline2: GoogleFonts.getFont("Inter",
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: const Color(0xFF575767),
+              decoration: TextDecoration.lineThrough),
+          subtitle1: GoogleFonts.getFont(
+            "Inter",
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: const Color(0xFF575767),
+          ),
+          bodyText1: GoogleFonts.getFont("Inter",
+              fontSize: 16,
+              color: const Color(0xFFDADADA),
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system,
       home: const FoldersPage(),
     );
   }
 }
-
