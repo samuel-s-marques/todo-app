9d54a96611b57ea5982e8f73a99b9d7aabf1c24b
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2022-01-09 14:22:15 -0300

    Create languages_page.dart

---
+++ lib/pages/languages_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguagesPage extends StatefulWidget {
+  const LanguagesPage({Key? key}) : super(key: key);
+
+  @override
+  _LanguagesPageState createState() => _LanguagesPageState();
+}
+
+class _LanguagesPageState extends State<LanguagesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
