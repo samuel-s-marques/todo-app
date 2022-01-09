e02ec90bbba0fac7223cb1b41c4fbcb195f06252
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2022-01-09 00:33:54 -0300

    Create settings_page.dart

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+
+      ),
+    );
+  }
+}
