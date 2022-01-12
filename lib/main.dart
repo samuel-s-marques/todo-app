ac6f7c0af25f6866ef39b6fe93b8856fb2c3d975
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2022-01-11 21:31:11 -0300

    Add new supporte locales and a localization provider

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'package:todoapp/utils/translate_preferences.dart';
 void main() async {
   var delegate = await LocalizationDelegate.create(
     fallbackLocale: 'en',
-    supportedLocales: ['en', 'pt', 'ja', 'es', 'ar'],
+    supportedLocales: ['en', 'pt', 'ja', 'es', 'ar', 'sv', 'fr'],
     preferences: TranslatePreferences()
   );
 
@@ -32,119 +32,121 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var localizationDelegate = LocalizedApp.of(context).delegate;
 
-    return ChangeNotifierProvider(
-      create: (_) => ThemeModel(),
-      child: Consumer<ThemeModel>(
-        builder: (context, ThemeModel themeNotifier, child) {
-          return MaterialApp(
-            title: 'To-do app',
-            localizationsDelegates: [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-              localizationDelegate
-            ],
-            supportedLocales: localizationDelegate.supportedLocales,
-            locale: localizationDelegate.currentLocale,
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-              brightness: Brightness.light,
-              dialogBackgroundColor: const Color(0xFFFAFAFA),
-              textTheme: TextTheme(
-                headline1: GoogleFonts.getFont(
-                  "Inter",
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: const Color(0xFF575767),
-                ),
-                headline2: GoogleFonts.getFont("Inter",
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: const Color(0xFFB9B9BE),
-                    decoration: TextDecoration.lineThrough),
-                subtitle1: GoogleFonts.getFont(
-                  "Inter",
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFFB9B9BE),
-                ),
-                headline3: GoogleFonts.getFont(
-                  "Inter",
-                  fontWeight: FontWeight.bold,
-                  color: const Color(0xFF0E0E11),
-                  fontSize: 20,
-                ),
-                subtitle2: GoogleFonts.getFont(
-                  "Inter",
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFF575757),
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            // 0xFF202020
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-              cardColor: const Color(0xFF141419),
-              scaffoldBackgroundColor: const Color(0xFF141419),
-              dialogBackgroundColor: const Color(0xFF303030),
-              inputDecorationTheme: InputDecorationTheme(
-                labelStyle: GoogleFonts.getFont(
-                  "Inter",
-                  fontSize: 16,
-                  color: const Color(0xFFB9B9BE),
-                ),
-                fillColor: Colors.blue,
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-              ),
-              textTheme: TextTheme(
-                headline1: GoogleFonts.getFont("Inter",
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: const Color(0xFFDADADA)),
-                headline2: GoogleFonts.getFont("Inter",
+    return LocalizationProvider(
+      state: LocalizationProvider.of(context).state,
+      child: ChangeNotifierProvider(
+        create: (_) => ThemeModel(),
+        child: Consumer<ThemeModel>(
+          builder: (context, ThemeModel themeNotifier, child) {
+            return MaterialApp(
+              title: 'To-do app',
+              localizationsDelegates: [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                localizationDelegate
+              ],
+              supportedLocales: localizationDelegate.supportedLocales,
+              locale: localizationDelegate.currentLocale,
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+                brightness: Brightness.light,
+                dialogBackgroundColor: const Color(0xFFFAFAFA),
+                textTheme: TextTheme(
+                  headline1: GoogleFonts.getFont(
+                    "Inter",
                     fontSize: 18,
                     fontWeight: FontWeight.w500,
                     color: const Color(0xFF575767),
-                    decoration: TextDecoration.lineThrough),
-                subtitle1: GoogleFonts.getFont(
-                  "Inter",
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFF575767),
-                ),
-                bodyText1: GoogleFonts.getFont(
-                  "Inter",
-                  fontSize: 16,
-                  color: const Color(0xFFDADADA),
-                ),
-                headline3: GoogleFonts.getFont(
-                  "Inter",
-                  fontWeight: FontWeight.bold,
-                  color: const Color(0xFFDADADA),
-                  fontSize: 20,
-                ),
-                subtitle2: GoogleFonts.getFont(
-                  "Inter",
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFF575757),
-                  fontSize: 16,
+                  ),
+                  headline2: GoogleFonts.getFont("Inter",
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xFFB9B9BE),
+                      decoration: TextDecoration.lineThrough),
+                  subtitle1: GoogleFonts.getFont(
+                    "Inter",
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFFB9B9BE),
+                  ),
+                  headline3: GoogleFonts.getFont(
+                    "Inter",
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFF0E0E11),
+                    fontSize: 20,
+                  ),
+                  subtitle2: GoogleFonts.getFont(
+                    "Inter",
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFF575757),
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            themeMode: themeNotifier.isDark ? ThemeMode.dark : ThemeMode.light,
-            initialRoute: "/folders",
-            routes: {
-              "/folders": (context) => const FoldersPage(),
-              "/tasks": (context) => const TasksPage(),
-              "/settings": (context) => const SettingsPage(),
-              "/languages": (context) => const LanguagesPage()
-            },
-          );
-        },
+              // 0xFF202020
+              darkTheme: ThemeData(
+                brightness: Brightness.dark,
+                cardColor: const Color(0xFF141419),
+                scaffoldBackgroundColor: const Color(0xFF141419),
+                dialogBackgroundColor: const Color(0xFF303030),
+                inputDecorationTheme: InputDecorationTheme(
+                  labelStyle: GoogleFonts.getFont(
+                    "Inter",
+                    fontSize: 16,
+                    color: const Color(0xFFB9B9BE),
+                  ),
+                  fillColor: Colors.blue,
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                textTheme: TextTheme(
+                  headline1: GoogleFonts.getFont("Inter",
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xFFDADADA)),
+                  headline2: GoogleFonts.getFont("Inter",
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xFF575767),
+                      decoration: TextDecoration.lineThrough),
+                  subtitle1: GoogleFonts.getFont(
+                    "Inter",
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFF575767),
+                  ),
+                  bodyText1: GoogleFonts.getFont(
+                    "Inter",
+                    fontSize: 16,
+                    color: const Color(0xFFDADADA),
+                  ),
+                  headline3: GoogleFonts.getFont(
+                    "Inter",
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFFDADADA),
+                    fontSize: 20,
+                  ),
+                  subtitle2: GoogleFonts.getFont(
+                    "Inter",
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFF575757),
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              themeMode: themeNotifier.isDark ? ThemeMode.dark : ThemeMode.light,
+              initialRoute: "/folders",
+              routes: {
+                "/folders": (context) => const FoldersPage(),
+                "/tasks": (context) => const TasksPage(),
+                "/settings": (context) => const SettingsPage(),
+                "/languages": (context) => const LanguagesPage()
+              },
+            );
+          },
+        ),
       ),
     );
   }
