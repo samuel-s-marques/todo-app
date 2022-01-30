608937b8994e6cb907412cf178e5504f31480902
Samuel Marques <samuel.s.marques@protonmail.com>
2022-01-30 20:06:36 -0300

    Refactor code

--- lib/pages/languages_page.dart
+++ lib/pages/languages_page.dart
@@ -17,20 +17,28 @@ class _LanguagesPageState extends State<LanguagesPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(translate("language.selection.title"),),
+        title: Text(
+          translate("language.selection.title"),
+        ),
       ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.only(top: 24.0, bottom: 16),
-            child: Text(translate("language.selection.message"), style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 20),),
+            child: Text(
+              translate("language.selection.message"),
+              style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 20),
+            ),
           ),
           ListView.builder(
             shrinkWrap: true,
             itemCount: localizedDelegate.supportedLocales.length,
             itemBuilder: (BuildContext context, int index) {
               return RadioListTile(
-                title: Text(translate("language.name.${localizedDelegate.supportedLocales[index].languageCode}"), style: Theme.of(context).textTheme.subtitle2!.copyWith(fontSize: 16),),
+                title: Text(
+                  translate("language.name.${localizedDelegate.supportedLocales[index].languageCode}"),
+                  style: Theme.of(context).textTheme.subtitle2!.copyWith(fontSize: 16),
+                ),
                 value: localizedDelegate.supportedLocales[index].languageCode,
                 groupValue: currentLocale,
                 onChanged: (value) {
@@ -42,7 +50,7 @@ class _LanguagesPageState extends State<LanguagesPage> {
             },
           ),
         ],
-      )
+      ),
     );
   }
 }
