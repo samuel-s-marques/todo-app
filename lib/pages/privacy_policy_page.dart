1e8d2691448400431107ac8bf896e42fd3b6ebaf
Samuel Marques <samuel.s.marques@protonmail.com>
2022-02-20 12:18:04 -0300

    Create privacy_policy_page.dart

---
+++ lib/pages/privacy_policy_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+
+class ShowPrivacyPolicyPage extends StatelessWidget {
+  const ShowPrivacyPolicyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future<String> getFileLines() async {
+      return await rootBundle.loadString("privacy_policy.md");
+    }
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: FutureBuilder(
+          future: getFileLines(),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            return Markdown(
+              data: snapshot.data ?? translate("settings_page.support_section.loading"),
+              selectable: true,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
