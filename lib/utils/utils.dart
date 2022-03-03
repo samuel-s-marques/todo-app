4322b2a015827adbe7b4defa2ebc23283fa463b8
Samuel Marques <samuel.s.marques@protonmail.com>
2022-03-03 14:35:42 -0300

    Add checkUpdate method

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
 import 'dart:math';
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:todoapp/models/update_model.dart';
 
 int generateId() {
   final _random = Random();
@@ -63,4 +68,27 @@ class ThemePreferences {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getBool(preferenceKey) ?? false;
   }
+}
+
+Future<Map<String, dynamic>> checkUpdate() async {
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  String version = packageInfo.version;
+  String buildNumber = packageInfo.buildNumber;
+  String currentVersion = version + "+" + buildNumber;
+
+  Uri url = Uri.parse("https://samuel-s-marques.github.io/assets/json/todo_app/app.json");
+  http.Response resp = await http.get(url);
+
+  if (resp.statusCode == 200) {
+    List<dynamic> updateObjects = jsonDecode(resp.body)['versions'] as List;
+    List<Update> updates = updateObjects.map((update) => Update.fromJson(update)).toList();
+
+    if (updates.last.version != currentVersion) {
+      return {"success": "there's a new update"};
+    }
+  } else {
+    return {"error": "internet connection error"};
+  }
+
+  return {"error": "without updates"};
 }
